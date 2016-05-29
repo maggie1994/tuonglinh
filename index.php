335f3605d6da3f9c440138f98c74da67ddee61d4
maggie <tuonglinhtong@gmail.com>
2016-05-29 19:01:42 +0700

    add home section

--- index.php
+++ index.php
@@ -1,20 +1,58 @@
 <html>
 	<head>
 		<title>Linh's Profile</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<!-- Font -->
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Signika:400,300,600,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Archivo+Black' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,500,300,100,200,800,700,600,900' rel='stylesheet' type='text/css'>
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<script src="jquery-1.10.2.min.js"></script>
 		<script src="js/main.js"></script>
-		<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 		
 	</head>
 	<body>
 		<!-- Page 01 -->
 		<div class="page">
-			
-		</div>
+			<h1 class="title-logo">My Profile!</h1>
+			<div class="cloud">
+				<div class="cloud-large">
+					<h5 class="hello">hello!</h5>
+					<h5 class="text-linh">I'm <span>LINH</span></h5>
+					<h6>what I like the most is to make <span>PRETTY WEBSITES</span></h6>
+				</div>
+				<div class="circle cirle-01"></div>
+				<div class="circle cirle-02"></div>
+				<div class="circle cirle-03"></div>
+				<div class="circle cirle-04"></div>
+				<div class="circle cirle-05"></div>
+				<div class="circle cirle-06"></div>
+				<div class="circle cirle-07"></div>
+			</div>
+			<div class="boat">
+				<img src="img/boat01.png" alt="">
+			</div>
+			<button class="scroll">
+				<h6>Scroll</h6>
+				<i class="fa fa-chevron-down" aria-hidden="true"></i>
+			</button>
+		</div><!-- Page 01 -->
+
+		<!-- Page 02 -->
+		<div class="page page-02">
+			<div class="bg-page-02"></div>
+			<div class="content">
+				<button class="scroll">
+					<h6>Scroll</h6>
+					<i class="fa fa-chevron-down" aria-hidden="true"></i>
+				</button>
+			</div>
+		</div><!-- Page 02 -->
 	</body>
 </html>
