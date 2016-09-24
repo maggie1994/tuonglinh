8a8486be8e70b327d181d3495d5960c3cb0cf4bb
maggie <tuonglinhtong@gmail.com>
2016-09-24 09:15:37 +0700

    responsive for website profile

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
 <head>
 	<title>Linh's Profile</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 	<link rel="icon" type="image/png" href="img/img-profile/air-balloon-orange.png">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" type="text/css" href="css/font.css">
